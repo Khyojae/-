1021d0b84194d44a31366574dd9fee9d1599a443
Khyojae <khjae201@gmail.com>
2025-02-05 19:22:20 +0900

    first commit

---
+++ config/db.js
@@ -0,0 +1,9 @@
+module.exports = {
+    host: '127.0.0.1',
+    user : 'anonyWebAdm',
+    port : '3306',
+    password : '1234',
+    database : 'anony_web',
+    authPlugin: 'mysql_native_password'
+    
+};
